3a4c254a4cf921d0aae7caf1100154984e3a1f84
Shiza Midhat <shizamidhat02@gmail.com>
2024-03-26 20:22:24 +0500

    Adding a Simple CLI Calculator

---
+++ index.ts
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+//printing a welcome message
+console.log("\n\tWelcome In \'ShizaMidhatCoding\' - CLI SIMPLE CALCULATOR\n");
+
+//Asking questions from user through inquirer
+
+let answers = await inquirer.prompt([
+    {message: "Enter First Number", type: "number", name: "firstNumber"},
+    {message: "Enter Second Number", type: "number", name: "secondNumber"},
+    {
+        message: "Select one operator to perform operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+
+//Conditional statements If-Else
+
+if(answers.operator === "Addition"){
+    console.log(answers.firstNumber + answers.secondNumber)
+}
+else if (answers.operator === "Subtraction"){
+    console.log(answers.firstNumber - answers.secondNumber)
+}
+else if (answers.operator === "Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber)
+}
+else if (answers.operator === "Division"){
+    console.log(answers.firstNumber / answers.secondNumber)
+}
+else{
+    console.log("Invalid Input")
+}
